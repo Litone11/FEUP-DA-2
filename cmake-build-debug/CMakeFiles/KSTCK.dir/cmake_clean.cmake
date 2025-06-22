8f4f6cbeb7ddbe67b43af44af75021d56418cce3
Luís Martins <luis.a110686@gmail.com>
2025-06-22 18:36:52 +0100

    Imported

---
+++ cmake-build-debug/CMakeFiles/KSTCK.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KSTCK.dir/algorithms.cpp.obj"
+  "CMakeFiles/KSTCK.dir/algorithms.cpp.obj.d"
+  "CMakeFiles/KSTCK.dir/main.cpp.obj"
+  "CMakeFiles/KSTCK.dir/main.cpp.obj.d"
+  "CMakeFiles/KSTCK.dir/reader.cpp.obj"
+  "CMakeFiles/KSTCK.dir/reader.cpp.obj.d"
+  "KSTCK.exe"
+  "KSTCK.exe.manifest"
+  "KSTCK.pdb"
+  "libKSTCK.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KSTCK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
